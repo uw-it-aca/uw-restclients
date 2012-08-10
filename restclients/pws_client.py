30690f64b7e48965bbad7d4e7b19d0c36074ef5f
jlaney@u.washington.edu <jlaney@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2012-08-10 18:09:46 +0000

    adds documentation, named exceptions, changes api of person_search
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@23 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/pws_client.py
+++ restclients/pws_client.py
@@ -4,7 +4,41 @@ import json
 import re
 
 
+class InvalidNetID(Exception):
+    """Exception for invalid netid."""
+    pass
+
+
+class InvalidRegID(Exception):
+    """Exception for invalid regid."""
+    pass
+
+
 class PWSClient(RestBase):
+    """
+    Client for retrieving person data from the UW Person Web Service. Configuration
+    parameters for this client are:
+
+    :PWS_URL:
+        The absolute URL of the PWS host. URL must include scheme and port (if not 80).
+        Ex. https://ucswseval1.cac.washington.edu:443
+
+    :PWS_CERT:
+        Path of a certficate file. Required for access to eval and production PWS.
+        Ex. /usr/local/ssl/foo.cert
+
+    :PWS_KEY:
+        Path of a public key file. Required for access to eval and production PWS.
+        Ex. /usr/local/ssl/foo.key
+
+    :PWS_TIMEOUT:
+        Socket timeout for each individual connection, can be a float. None disables timeout.
+
+    :PWS_LOG:
+        Path of a file where logging will be written.
+        Ex. /usr/local/logs/eval/pws.log
+
+    """
     URL_BASE = '/identity/v1'
 
     def __init__(self):
@@ -24,22 +58,56 @@ class PWSClient(RestBase):
         return json.loads(r.data)
 
     def get_person_by_regid(self, regid):
+        """
+        Retrieves person data for a person identified by the passed UWRegID.
+        """
         if not re.match(r'^[A-F0-9]{32}$', regid, re.I):
-            raise Exception("Invalid regid: " + regid)
+            raise InvalidRegID(regid)
 
         return self.get_json(self.URL_BASE + '/person/%s.json' % regid.upper())
 
     def get_person_by_netid(self, netid):
+        """
+        Retrieves person data for a person identified by the passed UWNetID.
+        """
         if not re.match(r'^([a-z]adm_)?[a-z][a-z0-9]{0,7}$', netid, re.I):
-            raise Exception("Invalid netid: " + netid)
+            raise InvalidNetID(netid)
 
-        data = self.get_persons({'uwnetid': netid.lower()})
+        data = self.person_search({'uwnetid': netid.lower()})
         if len(data):
             person_data = data[0].get('PersonURI')
             if person_data:
                 return self.get_person_by_regid(person_data.get('UWRegID'))
 
-    def get_persons(self, opts={}):
+    def person_search(self, opts={}):
+        """
+        Search for person data. Valid parameters for searching are:
+
+        :param uwregid:
+            UW RegID
+
+        :param uwnetid:
+            UW NetID
+
+        :param employee_id:
+            Employee Identification Number (HEPPS primary identifier; 9 digits)
+
+        :param student_number:
+            UW Student number in SDB (7 digits)
+
+        :param student_system_key:
+            Unique key used in SDB to identify records (9 digits)
+
+        :param development_id:
+            UW Development Office Advance DB identification (10 digits)
+
+        :param registered_surname:
+            Official surname
+
+        :param registered_first_middle_name:
+            All but surname of official records name
+
+        """
         fields = {
             'uwregid': '',
             'uwnetid': '',
