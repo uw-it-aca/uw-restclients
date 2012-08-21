011eb64ee1ab485964a4f84b3fbd050047fbe4c9
pmichaud@u.washington.edu <pmichaud@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2012-08-21 16:49:04 +0000

    pep8 / pylint
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@39 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/mock_http.py
+++ restclients/mock_http.py
@@ -1,6 +1,19 @@
+"""
+Contains objects used by the non-HTTP DAO implementations
+"""
+
 
 class MockHTTP(object):
+    """
+    An alternate object to HTTPResponse, for non-HTTP DAO
+    implementations to use.  Implements the API of HTTPResponse
+    as needed.
+    """
     status = 0
     body = ""
+
     def read(self):
+        """
+        Returns the document body of the request.
+        """
         return self.body
