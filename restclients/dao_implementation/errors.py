a10662ae6094318b08563619a175a47dbe0c81f1
pmichaud@u.washington.edu <pmichaud@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2012-08-21 16:44:26 +0000

    pep8 / pylint
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@38 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/dao_implementation/errors.py
+++ restclients/dao_implementation/errors.py
@@ -11,6 +11,12 @@ class Always404(object):
     Always404 will return a 404 for any URL given.
     """
     def getURL(self, url, headers):
+        """
+        This method takes a partial url, e.g. "/v1/person/123AAAAA" and
+        a dictionary of headers.
+
+        Returns an object that can be used like an HTTPResponse object.
+        """
         response = MockHTTP()
         response.status = 404
         response.body = "Not found"
@@ -23,6 +29,12 @@ class Always500(object):
     Always500 will return a 500 for any URL given.
     """
     def getURL(self, url, headers):
+        """
+        This method takes a partial url, e.g. "/v1/person/123AAAAA" and
+        a dictionary of headers.
+
+        Returns an object that can be used like an HTTPResponse object.
+        """
         response = MockHTTP()
         response.status = 500
         response.body = ""
