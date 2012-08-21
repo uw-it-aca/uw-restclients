7266cba8092d5c8a3c335056a84850ba30e8aa62
pmichaud@u.washington.edu <pmichaud@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2012-08-21 23:23:05 +0000

    A rough bit of mock data for registration search
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@47 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/sws.py
+++ restclients/sws.py
@@ -4,6 +4,8 @@ This is the interface for interacting with the Student Web Service.
 
 from restclients.dao import SWS_DAO
 from restclients.models import Term
+from restclients.exceptions import DataFailureException
+from urllib import urlencode
 import json
 import re
 
@@ -35,3 +37,23 @@ class SWS(object):
         term.last_final_exam_date = term_data["LastFinalExamDay"]
 
         return term
+
+    def registration_for_regid_and_term(self, regid, term):
+        """
+        Returns an array of restclients.Section objects, for the regid and term passed in.
+        """
+        dao = SWS_DAO()
+        url = "/student/v4/registration.json?" + urlencode({
+            'year': term.year,
+            'quarter': term.quarter,
+            'reg_id': regid,
+            'is_active': 'on',
+        })
+        response = dao.getURL(url, {"Accept": "application/json"})
+
+        if response.status != 200:
+            raise DataFailureException(url, response.status, response.read())
+
+        term_data = json.loads(response.data)
+        return term_data
+ 
