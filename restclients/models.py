a0400ff76c57fd97948c57b1613ca77ed8a84afc
pmichaud@u.washington.edu <pmichaud@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2012-08-23 20:55:24 +0000

    Getting most of the schedule for user/term in place.  Fake instructor data for now
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@48 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/models.py
+++ restclients/models.py
@@ -27,3 +27,111 @@ class Term(models.Model):
     class Meta:
         unique_together = ('year',
                            'quarter')
+
+class Section(models.Model):
+    term = models.ForeignKey(Term,
+                             on_delete=models.PROTECT)
+    curriculum_abbr = models.CharField(max_length=6,
+                                       db_index=True)
+    course_number = models.PositiveSmallIntegerField(db_index=True)
+    section_id = models.CharField(max_length=2,
+                                  db_index=True)
+    course_title = models.CharField(max_length=20)
+    course_campus = models.CharField(max_length=7)
+    section_type = models.CharField(max_length=2)
+    class_website_url = models.URLField(max_length=255,
+                              verify_exists=False)
+    sln = models.PositiveIntegerField()
+    summer_term = models.CharField(max_length=1)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    final_exam_date = models.DateField()
+    final_exam_start_time = models.TimeField()
+    final_exam_end_time = models.TimeField()
+    final_exam_building = models.CharField(max_length=5)
+    final_exam_room_number = models.CharField(max_length=5)
+    class Meta:
+        unique_together = ('term',
+                           'curriculum_abbr',
+                           'course_number',
+                           'section_id')
+
+    def json_data(self):
+        data = {
+            'curriculum_abbr': self.curriculum_abbr,
+            'course_number': self.course_number,
+            'section_id': self.section_id,
+            'course_title': self.course_title,
+            'course_campus': self.course_campus,
+            'class_website_url': self.class_website_url,
+            'sln': self.sln,
+            'summer_term': self.summer_term,
+            'start_date': '',
+            'end_date': '',
+            'meetings': []
+        }
+
+        for meeting in self.meetings:
+            data["meetings"].append(meeting.json_data())
+
+        return data
+
+class SectionMeeting(models.Model):
+    term = models.ForeignKey(Term,
+                             on_delete=models.PROTECT)
+    section = models.ForeignKey(Section,
+                                on_delete=models.PROTECT)
+    meeting_index = models.PositiveSmallIntegerField()
+    meeting_type = models.CharField(max_length=2)
+    building_to_be_arranged = models.BooleanField()
+    building = models.CharField(max_length=5)
+    room_to_be_arranged = models.BooleanField()
+    room_number = models.CharField(max_length=5)
+    days_to_be_arranged = models.BooleanField()
+    days_week = models.CharField(max_length=10)
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+#    instructor = models.ForeignKey(Instructor, on_delete=models.PROTECT)
+    last_verified = models.DateTimeField()
+    class Meta:
+        unique_together = ('term',
+                           'section',
+                           'meeting_index')
+
+    def json_data(self):
+        data = {
+            'index': self.meeting_index,
+            'type': self.meeting_type,
+            'days_tbd': self.days_to_be_arranged,
+            'days': self.days_week,
+            'start_time': self.start_time,
+            'end_time': self.end_time,
+            'building_tbd': self.building_to_be_arranged,
+            'building': self.building,
+            'room_tbd': self.room_to_be_arranged,
+            'room': self.room_number,
+            'instructors': [],
+            'instructor': {
+                'name': 'This isnt real',
+                'email': 'fake@fake.fake',
+                'phone': '206-...',
+            }
+        }
+        return data
+
+class ClassSchedule(models.Model):
+    user = models.ForeignKey(Person)
+    term = models.ForeignKey(Term,
+                             on_delete=models.PROTECT)
+
+    def json_data(self):
+        data = {
+            'year': self.term.year,
+            'quarter': self.term.quarter,
+            'sections': [],
+        }
+
+        for section in self.sections:
+            data["sections"].append(section.json_data())
+
+        return data
--- restclients/sws.py
+++ restclients/sws.py
@@ -3,7 +3,8 @@ This is the interface for interacting with the Student Web Service.
 """
 
 from restclients.dao import SWS_DAO
-from restclients.models import Term
+from restclients.models import Term, Section, SectionMeeting
+from restclients.models import ClassSchedule
 from restclients.exceptions import DataFailureException
 from urllib import urlencode
 import json
@@ -38,9 +39,9 @@ class SWS(object):
 
         return term
 
-    def registration_for_regid_and_term(self, regid, term):
+    def schedule_for_regid_and_term(self, regid, term):
         """
-        Returns an array of restclients.Section objects, for the regid and term passed in.
+        Returns a restclients.ClassSchedule for the regid and term passed in.
         """
         dao = SWS_DAO()
         url = "/student/v4/registration.json?" + urlencode({
@@ -55,5 +56,74 @@ class SWS(object):
             raise DataFailureException(url, response.status, response.read())
 
         term_data = json.loads(response.data)
-        return term_data
+
+        sections = []
+
+        for registration in term_data["Registrations"]:
+            reg_url = registration["Href"]
+
+            # Skip a step here, and go right to the course section resource
+            reg_url = re.sub('registration', 'course', reg_url)
+            reg_url = re.sub('^(.*?,.*?,.*?,.*?,.*?),.*', '\\1.json', reg_url)
+            reg_url = re.sub(',([^,]*).json', '/\\1.json', reg_url)
+            response = dao.getURL(reg_url, {"Accept":"application/json"})
+            section_data = json.loads(response.data)
+
+            section = Section()
+            section_term = Term()
+            section_term.year = section_data["Course"]["Year"]
+            section_term.quarter = section_data["Course"]["Quarter"]
+            section.term = section_term
+
+            section.curriculum_abbr = section_data["Course"]["CurriculumAbbreviation"]
+            section.course_number = section_data["Course"]["CourseNumber"]
+            section.section_id = section_data["SectionID"]
+            section.course_title = section_data["Course"]["CourseTitle"]
+            section.course_campus = section_data["CourseCampus"]
+            section.section_type = section_data["SectionType"]
+            section.class_website_url = section_data["ClassWebsiteUrl"]
+            section.sln = section_data["SLN"]
+            section.summer_term = section_data["SummerTerm"]
+
+            # These come from the Term resource
+            # section.start_date = ...
+            # section.end_date = ...
+            # final exam schedule...
+
+            section_meetings = []
+            for meeting_data in section_data["Meetings"]:
+                meeting = SectionMeeting()
+                meeting.meeting_index = meeting_data["MeetingIndex"]
+                meeting.meeting_type = meeting_data["MeetingType"]
+                meeting.building = meeting_data["Building"]
+                if meeting_data["BuildingToBeArranged"]:
+                    meeting.building_to_be_arranged = True
+                else:
+                    meeting.building_to_be_arranged = False
+
+                meeting.room_number = meeting_data["RoomNumber"]
+                if meeting_data["RoomToBeArranged"]:
+                    meeting.room_to_be_arranged = True
+                else:
+                    meeting.room_to_be_arranged = False
+
+                meeting.days_week = meeting_data["DaysOfWeek"]["Text"]
+                if meeting_data["DaysOfWeekToBeArranged"]:
+                    meeting.days_to_be_arranged = True
+                else:
+                    meeting.days_to_be_arranged = False
+
+                meeting.start_time = meeting_data["StartTime"]
+                meeting.end_time = meeting_data["EndTime"]
+
+                section_meetings.append(meeting)
+
+            section.meetings = section_meetings
+            sections.append(section)
+
+        schedule = ClassSchedule()
+        schedule.sections = sections
+        schedule.term = term
+
+        return schedule
  
