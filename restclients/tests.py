cad679bb04e2207243ef6dc09b82a74c3a444ea9
glee23@u.washington.edu <glee23@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2012-08-24 16:25:06 +0000

    Removing added code from tests.py that made it run outside of django app.
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@54 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/tests.py
+++ restclients/tests.py
@@ -19,5 +19,3 @@ from restclients.test.pws.err500.pws import PWSTest500
 from restclients.test.pws.invalid_dao import PWSTestInvalidDAO
 from restclients.test.pws.file_implementation.dao import PWSTestFileDAO
 
-if __name__ == '__main__':
-    unittest.main()
