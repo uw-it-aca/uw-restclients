93f050ed674ed5765b6931f47b40fdc268956ad4
pmichaud@u.washington.edu <pmichaud@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2012-08-29 23:27:36 +0000

    Adds a json_data method
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@99 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/models.py
+++ restclients/models.py
@@ -276,3 +276,16 @@ class Book(models.Model):
     is_required = models.BooleanField()
     notes = models.TextField()
     cover_image_url = models.CharField(max_length=2048)
+
+    def json_data(self):
+        data = {
+            'isbn': self.isbn,
+            'title': self.title,
+            'author': self.author,
+            'price': self.price,
+            'used_price': self.used_price,
+            'is_required': self.is_required,
+            'notes': self.notes,
+            'cover_image_url': self.cover_image_url,
+        }
+        return data
