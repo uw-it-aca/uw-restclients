ac5ba6da6529fdd6dd55fb788ac027b0277b419f
pmichaud@u.washington.edu <pmichaud@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2012-10-26 23:35:53 +0000

    Adding a setup.py, and a note in the requirements.txt, so restclients can be installed via pip
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@198 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='RestClients',
+      version='1.0',
+      description='UW RestClients',
+      install_requires=['Django==1.4', 'lxml==2.3.5', 'urllib3>=1.4'],
+     )
