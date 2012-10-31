764c38942b7762d4ded68c5f1a8e442547a9708a
pmichaud@u.washington.edu <pmichaud@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2012-10-31 18:26:58 +0000

    Adding boto to setup.py
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@209 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ from distutils.core import setup
 setup(name='RestClients',
       version='1.0',
       description='UW RestClients',
-      install_requires=['Django==1.4', 'lxml==2.3.5', 'urllib3>=1.4', 'twilio==3.4.1'],
+      install_requires=['Django==1.4', 'lxml==2.3.5', 'urllib3>=1.4', 'twilio==3.4.1', 'boto'],
      )
