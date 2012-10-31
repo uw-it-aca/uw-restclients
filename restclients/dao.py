bef12d38472cca154949a89e23c67802a08c5c63
pmichaud@u.washington.edu <pmichaud@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2012-10-31 17:43:09 +0000

    Refactoring the module from config code
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@207 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/dao.py
+++ restclients/dao.py
@@ -5,29 +5,33 @@ from restclients.dao_implementation.pws import File as PWSFile
 from restclients.dao_implementation.sws import File as SWSFile
 from restclients.dao_implementation.gws import File as GWSFile
 from restclients.dao_implementation.book import File as BookFile
+from restclients.dao_implementation.nws import File as NWSFile
+from restclients.dao_implementation.sms import Local as SMSLocal
 from restclients.dao_implementation.amazon_sqs import Local as SQSLocal
 from restclients.cache_implementation import NoCache
 
-
-class MY_DAO(object):
-    def _getCache(self):
-        if hasattr(settings, 'RESTCLIENTS_DAO_CACHE_CLASS'):
+class DAO_BASE(object):
+    def _getModule(self, settings_key, default_class):
+        if hasattr(settings, settings_key):
             # This is all taken from django's static file finder
-            module, attr = settings.RESTCLIENTS_DAO_CACHE_CLASS.rsplit('.', 1)
+            module, attr = getattr(settings, settings_key).rsplit('.', 1)
             try:
                 mod = import_module(module)
             except ImportError, e:
                 raise ImproperlyConfigured('Error importing module %s: "%s"' %
                                            (module, e))
             try:
-                CacheModule = getattr(mod, attr)
+                config_module = getattr(mod, attr)
             except AttributeError:
                 raise ImproperlyConfigured('Module "%s" does not define a '
                                    '"%s" class' % (module, attr))
-
-            return CacheModule()
+            return config_module()
         else:
-            return NoCache()
+            return default_class()
+
+class MY_DAO(DAO_BASE):
+    def _getCache(self):
+        return self._getModule('RESTCLIENTS_DAO_CACHE_CLASS', NoCache)
 
     def _getURL(self, service, url, headers):
         dao = self._getDAO()
@@ -55,23 +59,7 @@ class SWS_DAO(MY_DAO):
         return self._getURL('sws', url, headers)
 
     def _getDAO(self):
-        if hasattr(settings, 'RESTCLIENTS_SWS_DAO_CLASS'):
-            # This is all taken from django's static file finder
-            module, attr = settings.RESTCLIENTS_SWS_DAO_CLASS.rsplit('.', 1)
-            try:
-                mod = import_module(module)
-            except ImportError, e:
-                raise ImproperlyConfigured('Error importing module %s: "%s"' %
-                                           (module, e))
-            try:
-                DAOModule = getattr(mod, attr)
-            except AttributeError:
-                raise ImproperlyConfigured('Module "%s" does not define a '
-                                   '"%s" class' % (module, attr))
-
-            return DAOModule()
-        else:
-            return SWSFile()
+        return self._getModule('RESTCLIENTS_SWS_DAO_CLASS', SWSFile)
 
 
 class PWS_DAO(MY_DAO):
@@ -79,23 +67,7 @@ class PWS_DAO(MY_DAO):
         return self._getURL('pws', url, headers)
 
     def _getDAO(self):
-        if hasattr(settings, 'RESTCLIENTS_PWS_DAO_CLASS'):
-            # This is all taken from django's static file finder
-            module, attr = settings.RESTCLIENTS_PWS_DAO_CLASS.rsplit('.', 1)
-            try:
-                mod = import_module(module)
-            except ImportError, e:
-                raise ImproperlyConfigured('Error importing module %s: "%s"' %
-                                           (module, e))
-            try:
-                DAOModule = getattr(mod, attr)
-            except AttributeError:
-                raise ImproperlyConfigured('Module "%s" does not define a '
-                                   '"%s" class ' % (module, attr))
-
-            return DAOModule()
-        else:
-            return PWSFile()
+        return self._getModule('RESTCLIENTS_PWS_DAO_CLASS', PWSFile)
 
 
 class GWS_DAO(MY_DAO):
@@ -103,23 +75,7 @@ class GWS_DAO(MY_DAO):
         return self._getURL('gws', url, headers)
 
     def _getDAO(self):
-        if hasattr(settings, 'RESTCLIENTS_GWS_DAO_CLASS'):
-            # This is all taken from django's static file finder
-            module, attr = settings.RESTCLIENTS_GWS_DAO_CLASS.rsplit('.', 1)
-            try:
-                mod = import_module(module)
-            except ImportError, e:
-                raise ImproperlyConfigured('Error importing module %s: "%s"' %
-                                           (module, e))
-            try:
-                DAOModule = getattr(mod, attr)
-            except AttributeError:
-                raise ImproperlyConfigured('Module "%s" does not define a '
-                                   '"%s" class ' % (module, attr))
-
-            return DAOModule()
-        else:
-            return GWSFile()
+        return self._getModule('RESTCLIENTS_GWS_DAO_CLASS', GWSFile)
 
 
 class Book_DAO(MY_DAO):
@@ -127,23 +83,7 @@ class Book_DAO(MY_DAO):
         return self._getURL('books', url, headers)
 
     def _getDAO(self):
-        if hasattr(settings, 'RESTCLIENTS_BOOK_DAO_CLASS'):
-            # This is all taken from django's static file finder
-            module, attr = settings.RESTCLIENTS_BOOK_DAO_CLASS.rsplit('.', 1)
-            try:
-                mod = import_module(module)
-            except ImportError, e:
-                raise ImproperlyConfigured('Error importing module %s: "%s"' %
-                                           (module, e))
-            try:
-                DAOModule = getattr(mod, attr)
-            except AttributeError:
-                raise ImproperlyConfigured('Module "%s" does not define a '
-                                   '"%s" class ' % (module, attr))
-
-            return DAOModule()
-        else:
-            return BookFile()
+        return self._getModule('RESTCLIENTS_BOOK_DAO_CLASS', BookFile)
 
 
 class AmazonSQS_DAO(MY_DAO):
@@ -157,23 +97,7 @@ class AmazonSQS_DAO(MY_DAO):
         return res
 
     def _getDAO(self):
-        if hasattr(settings, 'AMAZON_SQS_DAO_CLASS'):
-            # This is all taken from django's static file finder
-            module, attr = settings.AMAZON_SQS_DAO_CLASS.rsplit('.', 1)
-            try:
-                mod = import_module(module)
-            except ImportError, e:
-                raise ImproperlyConfigured('Error importing module %s: "%s"' %
-                                           (module, e))
-            try:
-                DAOModule = getattr(mod, attr)
-            except AttributeError:
-                raise ImproperlyConfigured('Module "%s" does not define a '
-                                   '"%s" class ' % (module, attr))
-
-            return DAOModule()
-        else:
-            return SQSLocal()
+        return self._getModule('AMAZON_SQS_DAO_CLASS', SQSLocal)
 
 
 class SMS_DAO(MY_DAO):
@@ -186,23 +110,7 @@ class SMS_DAO(MY_DAO):
         return dao.send_message(message)
 
     def _getDAO(self):
-        if hasattr(settings, 'SMS_DAO_CLASS'):
-            # This is all taken from django's static file finder
-            module, attr = settings.SMS_DAO_CLASS.rsplit('.', 1)
-            try:
-                mod = import_module(module)
-            except ImportError, e:
-                raise ImproperlyConfigured('Error importing module %s: "%s"' %
-                                           (module, e))
-            try:
-                DAOModule = getattr(mod, attr)
-            except AttributeError:
-                raise ImproperlyConfigured('Module "%s" does not define a '
-                                   '"%s" class ' % (module, attr))
-
-            return DAOModule()
-        else:
-            return SMSLocal()
+        return self._getModule('SMS_DAO_CLASS', SMSLocal)
 
 
 class NWS_DAO(MY_DAO):
@@ -210,20 +118,4 @@ class NWS_DAO(MY_DAO):
         return self._getURL('nws', url, headers)
 
     def _getDAO(self):
-        if hasattr(settings, 'RESTCLIENTS_NWS_DAO_CLASS'):
-            # This is all taken from django's static file finder
-            module, attr = settings.RESTCLIENTS_NWS_DAO_CLASS.rsplit('.', 1)
-            try:
-                mod = import_module(module)
-            except ImportError, e:
-                raise ImproperlyConfigured('Error importing module %s: "%s"' %
-                                           (module, e))
-            try:
-                DAOModule = getattr(mod, attr)
-            except AttributeError:
-                raise ImproperlyConfigured('Module "%s" does not define a '
-                                   '"%s" class ' % (module, attr))
-
-            return DAOModule()
-        else:
-            return NWSFile()
+        return self._getModule('RESTCLIENTS_NWS_DAO_CLASS', NWSFile)
