04b368ed2ae416ac6e6d6850c26bbbd9a220b6f4
pmichaud@u.washington.edu <pmichaud@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2012-10-31 18:24:42 +0000

    Adding a live sqs implementation, and boto as a requirement
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@208 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/dao_implementation/amazon_sqs.py
+++ restclients/dao_implementation/amazon_sqs.py
@@ -2,7 +2,9 @@
 Contains Amazon SWS DAO implementations.
 """
 from restclients.models import MockAmazonSQSQueue
+from boto.sqs.connection import SQSConnection
 from django.db import IntegrityError
+from django.conf import settings
 
 
 class Local(object):
@@ -23,3 +25,28 @@ class Local(object):
             return queues[0]
 
         return None
+
+
+class Live(object):
+    """
+    This implements a connection to the Amazon SQS service.  Requires the
+    following configuration:
+
+    AMAZON_AWS_ACCESS_KEY
+    AMAZON_AWS_SECRET_KEY
+    """
+
+    def create_queue(self, queue_name):
+        conn = self._get_connection()
+        return conn.create_queue(queue_name)
+
+    def get_queue(self, queue_name):
+        conn = self._get_connection()
+        return conn.get_queue(queue_name)
+
+    def _get_connection(self):
+        access_key = settings.AMAZON_AWS_ACCESS_KEY
+        secret_key = settings.AMAZON_AWS_SECRET_KEY
+
+        conn = SQSConnection(access_key, secret_key)
+        return conn
