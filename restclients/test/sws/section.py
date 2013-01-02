20f7fdfa1e098828f8c3e6d4ce3677ad9cbffcbc
pmichaud@u.washington.edu <pmichaud@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2013-01-02 19:21:44 +0000

    Fixing the sws rest client section test data
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@344 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/test/sws/section.py
+++ restclients/test/sws/section.py
@@ -9,28 +9,15 @@ class SWSTestSectionData(TestCase):
                 RESTCLIENTS_SWS_DAO_CLASS='restclients.dao_implementation.sws.File'):
             sws = SWS()
 
-            section = sws.get_section_by_label('2012,summer,B BIO,180/A')
+            section = sws.get_section_by_label('2013,summer,B BIO,180/A')
             self.assertEquals(section.final_exam, None, "No final exame for B BIO 180")
 
-            section = sws.get_section_by_label('2012,summer,MATH,125/G')
+            section = sws.get_section_by_label('2013,summer,MATH,125/G')
             final_exam = section.final_exam
 
             self.assertEquals(final_exam.is_confirmed, False, "Final exame for Math 125 isn't confirmed")
             self.assertEquals(final_exam.no_exam_or_nontraditional, False, "Final exame for Math 125 isn't non-traditional")
-
-            section = sws.get_section_by_label('2012,summer,PHYS,121/A')
-            final_exam = section.final_exam
-
-            self.assertEquals(final_exam.is_confirmed, False, "Final exame for Phys 121 isn't confirmed")
-            self.assertEquals(final_exam.no_exam_or_nontraditional, True, "Final exame for Phys 121 is non-traditional")
-
-            section = sws.get_section_by_label('2012,summer,TRAIN,100/A')
-            final_exam = section.final_exam
-
-            self.assertEquals(final_exam.is_confirmed, True, "Final exame for Train 100 is confirmed")
-            self.assertEquals(final_exam.no_exam_or_nontraditional, False, "Final exame for Train 100 isn't non-traditional")
-
-            section = sws.get_section_by_label('2012,summer,TRAIN,101/A')
+            section = sws.get_section_by_label('2013,summer,TRAIN,101/A')
             final_exam = section.final_exam
 
             self.assertEquals(final_exam.is_confirmed, True, "Final exame for Train 101 is confirmed")
@@ -41,13 +28,13 @@ class SWSTestSectionData(TestCase):
             start = final_exam.start_date
             end = final_exam.end_date
 
-            self.assertEquals(start.year, 2012)
+            self.assertEquals(start.year, 2013)
             self.assertEquals(start.month, 6)
             self.assertEquals(start.day, 2)
             self.assertEquals(start.hour, 13)
             self.assertEquals(start.minute, 30)
 
-            self.assertEquals(end.year, 2012)
+            self.assertEquals(end.year, 2013)
             self.assertEquals(end.month, 6)
             self.assertEquals(end.day, 2)
             self.assertEquals(end.hour, 16)
@@ -61,25 +48,7 @@ class SWSTestSectionData(TestCase):
             sws = SWS()
         
             #Valid data, shouldn't throw any exceptions
-            sws.get_section_by_label('2012,summer,TRAIN,100/A')
-            sws.get_section_by_label('2012,summer,TRAIN,101/A')
-            sws.get_section_by_label('2012,summer,TRAIN,102/A')
-            sws.get_section_by_label('2012,summer,TRAIN,103/A')
-            sws.get_section_by_label('2012,summer,TRAIN,104/A')
-            sws.get_section_by_label('2012,summer,PHYS,121/A')
-            sws.get_section_by_label('2012,summer,PHYS,121/AC')
-            sws.get_section_by_label('2012,summer,PHYS,121/AQ')
-            sws.get_section_by_label('2012,summer,TEST,098/T')
-            sws.get_section_by_label('2012,summer,B BIO,180/A')
-            sws.get_section_by_label('2012,summer,B BIO,180/AA')
-            sws.get_section_by_label('2012,summer,EDC&I,461/A')
-            sws.get_section_by_label('2012,summer,CSS,161/A')
-            sws.get_section_by_label('2012,summer,T BUS,310/A')
-            sws.get_section_by_label('2012,summer,MATH,125/G')
-            sws.get_section_by_label('2012,summer,MATH,125/GA')
-            sws.get_section_by_label('2012,summer,TEDADM,572/A')
-            sws.get_section_by_label('2012,summer,TMATH,110/A')
-            sws.get_section_by_label('2012,summer,ELCBUS,451/A')
+            sws.get_section_by_label('2013,summer,TRAIN,100/A')
 
             #Invalid data, should throw exceptions
             self.assertRaises(InvalidSectionID, 
@@ -163,48 +132,8 @@ class SWSTestSectionData(TestCase):
                 RESTCLIENTS_SWS_DAO_CLASS='restclients.dao_implementation.sws.File'):
             sws = SWS()
             #Valid data, shouldn't throw any exceptions
-            section = sws.get_section_by_label('2012,summer,TRAIN,100/A')
+            section = sws.get_section_by_label('2013,summer,TRAIN,100/A')
             sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,TRAIN,101/A')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,TRAIN,102/A')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,TRAIN,103/A')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,TRAIN,104/A')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,PHYS,121/A')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,PHYS,121/AC')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,PHYS,121/AQ')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,B BIO,180/A')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,B BIO,180/AA')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,CSS,161/A')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,T BUS,310/A')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,TEST,098/T')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,TRAIN,101/A')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,MATH,125/G')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,MATH,125/GA')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,EDC&I,461/A')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,TEDADM,572/A')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,TMATH,110/A')
-            sws.get_linked_sections(section)
-            section = sws.get_section_by_label('2012,summer,ELCBUS,451/A')
-            sws.get_linked_sections(section)
-            
-
 
             #Invalid data, should throw exceptions
             section.linked_section_urls = ['']
