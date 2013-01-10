10ca4efb53900ccd20a9c5387360954dbee5c1bd
pmichaud@u.washington.edu <pmichaud@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2013-01-10 20:00:15 +0000

    Adding an sms implementation that emails the sms content
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@362 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/dao_implementation/sms.py
+++ restclients/dao_implementation/sms.py
@@ -6,6 +6,8 @@ from restclients.models import SMSRequest, SMSResponse
 from restclients.exceptions import DataFailureException
 from restclients.dao_implementation.twilio_wrapper import Twilio
 from django.db import IntegrityError
+from django.core.mail import EmailMessage
+from django.conf import settings
 
 
 class Local(object):
@@ -32,6 +34,28 @@ class Local(object):
 
         return response
 
+class Email(object):
+    """
+    This implementation sends an email with the content of the SMS, for
+    testing purposes
+    """
+    def create_message(self, to, body):
+        message = SMSRequest()
+        message.to = to
+        message.body = body
+        return message
+
+    def send_message(self, message):
+        content = "Phone number: %s\nMessage:\n\n%s" % (message.to, message.body)
+        email = EmailMessage('SMS Message', content, settings.RESTCLIENTS_SMS_EMAIL_TO)
+        email.to = [settings.RESTCLIENTS_SMS_EMAIL_TO]
+        email.send()
+
+        response = SMSResponse()
+        response.to = message.to
+        response.body = message.body
+        response.status = "queued"
+        response.rid = "email_sms"
 
 class Live(object):
     """
