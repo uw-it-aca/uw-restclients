1b3eac763b85f034302243dde088f56a0556f557
jlaney@u.washington.edu <jlaney@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2013-10-03 19:23:27 +0000

    adds clients for canvas reports and analytics resources
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@506 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

---
+++ restclients/canvas/analytics.py
@@ -0,0 +1,93 @@
+from django.conf import settings
+from restclients.canvas import Canvas
+from restclients.dao import Canvas_DAO
+from restclients.exceptions import DataFailureException
+import json
+
+
+class Analytics(Canvas):
+    def get_activity_by_account(self, account_id, term_id):
+        """
+        Returns participation data for the given account_id and term_id.
+
+        https://canvas.instructure.com/doc/api/analytics.html#method.analytics_api.department_participation
+        """
+        url = "/api/v1/accounts/sis_account_id:%s/analytics/terms/sis_term_id:%s/activity.json" % (
+            account_id, term_id)
+        return self._get_resource(url)
+
+    def get_grades_by_account(self, account_id, term_id):
+        """
+        Returns grade data for the given account_id and term_id.
+
+        https://canvas.instructure.com/doc/api/analytics.html#method.analytics_api.department_grades
+        """
+        url = "/api/v1/accounts/sis_account_id:%s/analytics/terms/sis_term_id:%s/grades.json" % (
+            account_id, term_id)
+        return self._get_resource(url)
+
+    def get_statistics_by_account(self, account_id, term_id):
+        """
+        Returns statistics for the given account_id and term_id.
+
+        https://canvas.instructure.com/doc/api/analytics.html#method.analytics_api.department_statistics
+        """
+        url = "/api/v1/accounts/sis_account_id:%s/analytics/terms/sis_term_id:%s/statistics.json" % (
+            account_id, term_id)
+        return self._get_resource(url)
+
+    def get_activity_by_course(self, course_id):
+        """
+        Returns participation data for the given course_id.
+
+        https://canvas.instructure.com/doc/api/analytics.html#method.analytics_api.course_participation
+        """
+        url = "/api/v1/courses/sis_course_id:%s/analytics/activity.json" % course_id
+        return self._get_resource(url)
+
+    def get_assignments_by_course(self, course_id):
+        """
+        Returns assignment data for the given course_id.
+
+        https://canvas.instructure.com/doc/api/analytics.html#method.analytics_api.course_assignments
+        """
+        url = "/api/v1/courses/sis_course_id:%s/analytics/assignments.json" % course_id
+        return self._get_resource(url)
+
+    def get_student_summaries_by_course(self, course_id):
+        """
+        Returns per-student data for the given course_id.
+
+        https://canvas.instructure.com/doc/api/analytics.html#method.analytics_api.course_student_summaries
+        """
+        url = "/api/v1/courses/sis_course_id:%s/analytics/student_summaries" % course_id
+        return self._get_resource(url)
+
+    def get_student_activity_for_course(self, user_id, course_id):
+        """
+        Returns student activity data for the given user_id and course_id.
+
+        https://canvas.instructure.com/doc/api/analytics.html#method.analytics_api.student_in_course_participation
+        """
+        url = "/api/v1/sis_course_id:courses/%s/analytics/users/sis_user_id:%s/activity.json" % (
+            course_id, user_id)
+        return self._get_resource(url)
+
+    def get_student_assignments_for_course(self, user_id, course_id):
+        """
+        Returns student assignment data for the given user_id and course_id.
+
+        https://canvas.instructure.com/doc/api/analytics.html#method.analytics_api.student_in_course_assignments
+        """
+        url = "/api/v1/courses/sis_course_id:%s/analytics/users/sis_user_id:%s/assignments" % (
+            course_id, user_id)
+        return self._get_resource(url)
+
+    def get_student_messaging_for_course(self, user_id, course_id):
+        """
+        Returns student messaging data for the given user_id and course_id.
+        https://canvas.instructure.com/doc/api/analytics.html#method.analytics_api.student_in_course_messaging
+        """
+        url = "/api/v1/courses/sis_course_id:%s/analytics/users/sis_user_id:%s/communication" % (
+            course_id, user_id)
+        return self._get_resource(url)
---
+++ restclients/canvas/reports.py
@@ -0,0 +1,114 @@
+from django.conf import settings
+from restclients.canvas import Canvas
+from restclients.dao import Canvas_DAO
+from restclients.exceptions import DataFailureException
+from restclients.models.canvas import Report, ReportType
+import json
+
+
+class Reports(Canvas):
+    def get_available_reports(self):
+        """
+        Returns the list of reports for the current context.
+        """
+        data = self._get_resource("/api/v1/accounts/%s/reports.json" % (
+                                  settings.RESTCLIENTS_CANVAS_ACCOUNT_ID))
+
+        report_types = []
+        for datum in data:
+            report_type = ReportType()
+            report_type.name = datum["report"]
+            report_type.title = datum["title"]
+            report_type.parameters = datum["parameters"]
+            if datum["last_run"] is not None:
+                report = self._report_from_json(datum["last_run"])
+                report_type.last_run = report
+
+            report_types.append(report_type)
+
+        return report_types
+
+    def get_reports_by_type(self, report_type):
+        """
+        Shows all reports of the passed report_type that have been run
+        for the account.
+        """
+        data = self._get_resource("/api/v1/accounts/%s/reports/%s.json" % (
+                                  settings.RESTCLIENTS_CANVAS_ACCOUNT_ID,
+                                  report_type))
+
+        reports = []
+        for datum in data:
+            reports.append(self._report_from_json(datum))
+
+        return reports
+
+    def create_report(self, report_type, parameters={}):
+        """
+        Generates a report instance for the account.
+        """
+        dao = Canvas_DAO()
+
+        url = "/api/v1/accounts/%s/reports/%s.json" % (
+            settings.RESTCLIENTS_CANVAS_ACCOUNT_ID, report_type)
+        headers = {"Accept": "application/json",
+                   "Content-Type": "application/json"}
+        body = json.dumps(parameters)
+
+        response = dao.postURL(url, headers, body)
+
+        if response.status != 200:
+            raise DataFailureException(url, response.status, response.data)
+
+        report_data = json.loads(response.data)
+
+        return self._report_from_json(report_data)
+
+    def get_report_status(self, report):
+        """
+        Returns the status of a report.
+        """
+        dao = Canvas_DAO()
+
+        url = "/api/v1/accounts/%s/reports/%s/%s.json" % (
+            settings.RESTCLIENTS_CANVAS_ACCOUNT_ID, report.type,
+            report.report_id)
+
+        response = dao.getURL(url, {"Accept": "application/json"})
+
+        if response.status != 200:
+            raise DataFailureException(url, response.status, response.data)
+
+        report_data = json.loads(response.data)
+
+        return self._report_from_json(report_data)
+
+    def delete_report(self, report):
+        """
+        Deletes a generated report instance.
+        """
+        dao = Canvas_DAO()
+
+        url = "/api/v1/accounts/%s/reports/%s/%s.json" % (
+            settings.RESTCLIENTS_CANVAS_ACCOUNT_ID, report.type,
+            report.report_id)
+
+        response = dao.deleteURL(url, {"Accept": "application/json"})
+
+        if response.status != 200:
+            raise DataFailureException(url, response.status, response.data)
+
+        report_data = json.loads(response.data)
+
+        return self._report_from_json(report_data)
+
+    def _report_from_json(self, data):
+        report = Report()
+        report.report_id = data["id"]
+        report.type = data["report"]
+        report.url = data["file_url"]
+        report.status = data["status"]
+        report.progress = data["progress"]
+        report.parameters = data["parameters"]
+
+        return report
