e5390e58c4b865850b1642159e28de9e982a90aa
pmichaud@u.washington.edu <pmichaud@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2013-12-16 23:49:03 +0000

    Adding spring quarter for javerage, and json_data methods
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@638 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/models/catalyst.py
+++ restclients/models/catalyst.py
@@ -8,9 +8,29 @@ class CourseGradeData(models.Model):
     total_score = models.CharField(max_length=10)
     url = models.CharField(max_length=250)
 
+    def json_data(self):
+        data = {
+            "name": self.name,
+            "url": self.url,
+            "class_grade": self.class_grade,
+            "total_score": self.total_score,
+            "assignments": []
+        }
+
+        for assignment in self.items:
+            data["assignments"].append(assignment.json_data())
+
+        return data
 
 class CourseGradeItem(models.Model):
     name = models.CharField(max_length=250)
     score = models.CharField(max_length=200)
     url = models.CharField(max_length=250)
+
+    def json_data(self):
+        return {
+            "name": self.name,
+            "score": self.score,
+            "url": self.url,
+        }
 
