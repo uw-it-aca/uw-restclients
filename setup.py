2b38567089125c033a94df43067b7970265c2704
pmichaud@u.washington.edu <pmichaud@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2013-12-17 01:00:05 +0000

    Dropping the version spec on django
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@642 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ from distutils.core import setup
 setup(name='RestClients',
       version='1.0',
       description='UW RestClients',
-      install_requires=['Django==1.4', 'lxml==2.3.5', 'urllib3>=1.4', 'twilio==3.4.1', 'boto', 'simplejson>=2.1', 'djangorestframework>=2.0', 'jsonpickle>=0.4.0', 'ordereddict>=1.1', 'python-dateutil>=2.1', 'unittest2>=0.5.1', 'pytz'],
+      install_requires=['Django', 'lxml==2.3.5', 'urllib3>=1.4', 'twilio==3.4.1', 'boto', 'simplejson>=2.1', 'djangorestframework>=2.0', 'jsonpickle>=0.4.0', 'ordereddict>=1.1', 'python-dateutil>=2.1', 'unittest2>=0.5.1', 'pytz'],
      )
