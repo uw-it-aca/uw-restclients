295a9a05fe6ac02cd99e8346e85a8b06fe5aca33
pmichaud@u.washington.edu <pmichaud@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2013-12-30 19:44:49 +0000

    Making it so mock data resources can be pulled from any installed app
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@665 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/dao_implementation/mock.py
+++ restclients/dao_implementation/mock.py
@@ -1,15 +1,39 @@
 from restclients.mock_http import MockHTTP
 from os.path import abspath, dirname
+from django.conf import settings
+from django.utils.importlib import import_module
+import sys
+import os
 import json
+import logging
 
 """
 A centralized the mock data access
 """
+# Based on django.template.loaders.app_directories
+fs_encoding = sys.getfilesystemencoding() or sys.getdefaultencoding()
+app_resource_dirs = []
+for app in settings.INSTALLED_APPS:
+    try:
+        mod = import_module(app)
+    except ImportError, e:
+        raise ImproperlyConfigured('ImportError %s: %s' % (app, e.args[0]))
 
+    resource_dir = os.path.join(os.path.dirname(mod.__file__), 'resources')
+    if os.path.isdir(resource_dir):
+        # Cheating, to make sure our resources are overridable
+        data = {
+            'path': resource_dir.decode(fs_encoding),
+            'app': app,
+        }
+        if app == 'restclients':
+            app_resource_dirs.append(data)
+        else:
+            app_resource_dirs.insert(0, data)
 
 def get_mockdata_url(service_name, implementation_name,
-                     url, headers,
-                     dir_base = dirname(__file__)):
+                     url, headers):
+
     """
     :param service_name:
         possible "sws", "pws", "book", "hfs", etc.
@@ -17,22 +41,53 @@ def get_mockdata_url(service_name, implementation_name,
         possible values: "file", etc.
     """
 
+    dir_base = dirname(__file__)
+
+
+
     RESOURCE_ROOT = abspath(dir_base + "/../resources/" +
                             service_name + "/" + implementation_name)
+
+    file_path = None
+    success = False
+
+    for resource_dir in app_resource_dirs:
+        response = _load_resource_from_path(resource_dir, service_name,
+                                            implementation_name, url, headers)
+
+        if response:
+            return response
+
+    # If no response has been found in any installed app, return a 404
+    response = MockHTTP()
+    response.status = 404
+    return response
+
+def _load_resource_from_path(resource_dir, service_name, implementation_name,
+                                url, headers):
+
+    RESOURCE_ROOT = os.path.join(resource_dir['path'],
+                                    service_name,
+                                    implementation_name)
+    app = resource_dir['app']
+
     if url == "///":
         # Just a placeholder to put everything else in an else.
         # If there are things that need dynamic work, they'd go here
         pass
     else:
         try:
-            handle = open(RESOURCE_ROOT + url)
+            file_path = RESOURCE_ROOT + url
+            handle = open(file_path)
         except IOError:
             try:
-                handle = open(RESOURCE_ROOT + url + "/index.html")
+                file_path = RESOURCE_ROOT + url + "/index.html"
+                handle = open(file_path)
             except IOError:
-                response = MockHTTP()
-                response.status = 404
-                return response
+                return
+
+        logger = logging.getLogger(__name__)
+        logger.info("URL: %s; App: %s; File: %s" % (url, app, file_path))
 
         response = MockHTTP()
         response.status = 200
@@ -47,6 +102,7 @@ def get_mockdata_url(service_name, implementation_name,
 
         return response
 
+    
 
 def post_mockdata_url(service_name, implementation_name,
                      url, headers, body,
