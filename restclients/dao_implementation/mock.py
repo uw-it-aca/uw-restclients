5b4ae817d81e93be3c4c5180eb823a8b94bbe9b9
pmichaud@u.washington.edu <pmichaud@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2014-01-02 19:58:21 +0000

    Making it so the .http-headers can includes headers and a status code
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@670 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/dao_implementation/mock.py
+++ restclients/dao_implementation/mock.py
@@ -98,9 +98,21 @@ def _load_resource_from_path(resource_dir, service_name, implementation_name,
 
         try:
             headers = open(handle.name + '.http-headers')
-            response.headers = dict(response.headers.items() + json.loads(headers.read()).items())
+            file_values = json.loads(headers.read())
+
+            if "headers" in file_values:
+                response.headers = dict(response.headers.items() + file_values['headers'].items())
+
+                if 'status' in file_values:
+                    response.status = file_values['status']
+
+            else:
+                response.headers = dict(response.headers.items() + json.loads(headers.read()).items())
+
         except IOError:
             pass
+        except Exception as ex:
+            print "Error: ", ex
 
         return response
 
