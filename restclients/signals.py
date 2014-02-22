57c749018b5dbe19225bb6689737f57f55f0b30a
devights@u.washington.edu <devights@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2014-02-22 00:50:00 +0000

    Fixing sender attrib in signal
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@703 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/signals.py
+++ restclients/signals.py
@@ -4,15 +4,15 @@ from django.utils import timezone
 import django.dispatch
 
 
-rest_request = django.dispatch.Signal(providing_args=['url', 'request_time', 'hostname'])
+rest_request = django.dispatch.Signal(providing_args=['url', 'request_time', 'hostname', 'sender'])
 
 
 def rest_request_receiver(sender, **kwargs):
     url = kwargs.get('url', None)
     request_time = kwargs.get('request_time', None)
-    service = kwargs.get('service', None)
+    service = kwargs.get('sender', None)
     hostname = kwargs.get('hostname', None)
-    dispatch('%s_request_interval' % service, RawEvent.INTERVAL, timezone.now(), request_time, url, hostname)
+    dispatch('%s_request_interval' % sender, RawEvent.INTERVAL, timezone.now(), request_time, url, hostname)
 
 def get_signal():
     return rest_request
