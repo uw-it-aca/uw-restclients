65b8f370a10dc353065027bd7c0fd230d54f11c0
devights@u.washington.edu <devights@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2014-02-22 00:38:26 +0000

    Updates signal
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@702 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/signals.py
+++ restclients/signals.py
@@ -1,3 +1,18 @@
+from status_app.dispatch import dispatch
+from status_app.models import RawEvent
+from django.utils import timezone
 import django.dispatch
 
-rest_request = django.dispatch.Signal(providing_args=['url','request_time', 'hostname'])
+
+rest_request = django.dispatch.Signal(providing_args=['url', 'request_time', 'hostname'])
+
+
+def rest_request_receiver(sender, **kwargs):
+    url = kwargs.get('url', None)
+    request_time = kwargs.get('request_time', None)
+    service = kwargs.get('service', None)
+    hostname = kwargs.get('hostname', None)
+    dispatch('%s_request_interval' % service, RawEvent.INTERVAL, timezone.now(), request_time, url, hostname)
+
+def get_signal():
+    return rest_request
