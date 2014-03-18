1f2f15b1b8fabb0b7df225e22dd57980bc95e809
fanglin@u.washington.edu <fanglin@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2014-03-18 22:23:28 +0000

    Resolve MUWM-1238.
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@745 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/test/sws/compatible.py
+++ restclients/test/sws/compatible.py
@@ -1,10 +1,19 @@
 from django.test import TestCase
 from django.conf import settings
 from datetime import datetime, timedelta
-from restclients.sws import SWS
+from restclients.sws import SWS, encode_section_label, get_resource
 from restclients.models.sws import Term, Curriculum, Person, College, Department
 from restclients.exceptions import DataFailureException
 from restclients.exceptions import InvalidSectionURL
+
+class UtilFunctionTest(TestCase):
+    def test_encode_section_label(self):
+        self.assertEquals(encode_section_label('2013,winter,C LIT,396/A'),
+                          '2013,winter,C%20LIT,396/A')
+
+    def test_get_resource(self):
+        self.assertEquals(get_resource('/student/v4/campus.json'),
+                          "{u'PageSize': u'10', u'Campuses': [{u'CampusShortName': u'BOTHELL', u'Href': u'/student/v4/campus/BOTHELL.json', u'CampusName': u'UW Bothell', u'CampusFullName': u'UNIVERSITY OF WASHINGTON BOTHELL'}, {u'CampusShortName': u'SEATTLE', u'Href': u'/student/v4/campus/SEATTLE.json', u'CampusName': u'UW Seattle', u'CampusFullName': u'UNIVERSITY OF WASHINGTON SEATTLE'}, {u'CampusShortName': u'TACOMA', u'Href': u'/student/v4/campus/TACOMA.json', u'CampusName': u'UW Tacoma', u'CampusFullName': u'UNIVERSITY OF WASHINGTON TACOMA'}], u'Next': None, u'Current': {u'Href': u'/student/v4/campus.json&page_start=1&page_size=10'}, u'TotalCount': 3, u'PageStart': u'1', u'Previous': None}")
 
 class SWSTest(TestCase):
     def test_mock_data_fake_grading_window(self):
@@ -15,14 +24,14 @@ class SWSTest(TestCase):
             sws = SWS()
 
             # backwards compatible for term
-            term = sws.get_current_term()
-            self.assertEquals(term.year, 2013)
-            self.assertEquals(term.quarter, 'spring')
-                
             term = sws.get_term_by_year_and_quarter(2013, 'spring')
             self.assertEquals(term.year, 2013)
             self.assertEquals(term.quarter, 'spring')
 
+            term = sws.get_current_term()
+            self.assertEquals(term.year, 2013)
+            self.assertEquals(term.quarter, 'spring')
+                
             prev_term = sws.get_previous_term()
             self.assertEquals(prev_term.year, 2013)
             self.assertEquals(prev_term.quarter, 'winter')
@@ -44,7 +53,7 @@ class SWSTest(TestCase):
             joint_sections = sws.get_joint_sections(section)
             self.assertEquals(len(joint_sections), 1)
 
-            section = sws.get_section_by_label('2013,summer,TRAIN,100/A')
+            section = sws.get_section_by_url('/student/v4/course/2013,summer,TRAIN,100/A.json')
             sws.get_linked_sections(section)
             section.linked_section_urls = ['2012,summer,TRAIN,100/A']
             self.assertRaises(InvalidSectionURL,
@@ -63,30 +72,39 @@ class SWSTest(TestCase):
             sections = sws.get_sections_by_curriculum_and_term(curriculum, term)
             self.assertEquals(len(sections), 2)
 
+            # backwards compatible for section_status
             section_status = sws.get_section_status_by_label(
                 '2012,autumn,CSE,100/W')
             self.assertEquals(section_status.sln, 12588)
 
+            # backwards compatible for registration
             section = sws.get_section_by_label('2013,winter,DROP_T,100/A')
+            registrations = sws.get_active_registrations_for_section(section)
+            self.assertEquals(len(registrations), 0)
             registrations = sws.get_all_registrations_for_section(section)
             self.assertEquals(len(registrations), 1)
 
             term = sws.get_current_term()
             sws.schedule_for_regid_and_term('9136CCB8F66711D5BE060004AC494FFE',
                                             term)
+            # backwards compatible for enrollment
             grades = sws.grades_for_regid_and_term('9136CCB8F66711D5BE060004AC494FFE', term)
             self.assertEquals(grades.user.uwnetid, "javerage")
 
+            # backwards compatible for campus
             campuses = sws.get_all_campuses()
             self.assertEquals(len(campuses), 3)
 
+            # backwards compatible for college
             colleges = sws.get_all_colleges()
             self.assertEquals(len(colleges), 20)
 
+            # backwards compatible for department
             college = College(label="MED")
             depts = sws.get_departments_for_college(college)
             self.assertEquals(len(depts), 30)
 
+            # backwards compatible for curriculum
             department = Department(label="EDUC")
             curricula = sws.get_curricula_for_department(department)
             self.assertEquals(len(curricula), 7)
