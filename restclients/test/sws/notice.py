5ff805a7373426e84b7e1fd8af774042dce5d9c7
fanglin@u.washington.edu <fanglin@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2014-05-09 19:26:22 +0000

    Added tests for various date values.
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@786 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/test/sws/notice.py
+++ restclients/test/sws/notice.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.conf import settings
 from restclients.sws.notice import get_notices_by_regid
-import datetime
+from datetime import date, datetime, timedelta
 
 class SWSNotice(TestCase):
     def test_notice_resource(self):
@@ -12,15 +12,34 @@ class SWSNotice(TestCase):
             notices = get_notices_by_regid("9136CCB8F66711D5BE060004AC494FFE")
             self.assertEquals(len(notices), 13)
 
+            today = date.today()
+            yesterday = today - timedelta(days=1)
+            tomorrow = today + timedelta(days=1)
+            week = today + timedelta(days=2)
+            next_week = today + timedelta(weeks=1)
+            future = today + timedelta(weeks=3)
+            future_end = today + timedelta(weeks=5)
+
+            notice = notices[0]
+            self.assertEquals(notice.notice_category, "StudentALR")
+            self.assertEquals(notice.notice_type, "IntlStuCheckIn")
+            attribute = notice.attributes[1]
+            self.assertEquals(attribute.name, "Date")
+            self.assertEquals(attribute.data_type, "date")
+            self.assertEquals(attribute.get_value(), yesterday.strftime("%Y-%m-%d"))
+
+
             notice = notices[1]
             self.assertEquals(notice.notice_category, "StudentDAD")
             self.assertEquals(notice.notice_type, "QtrBegin")
-            self.assertEquals(notice.notice_content, "Summer quarter begins <b>June 23, 2014</b>")
+            self.assertEquals(notice.notice_content,
+                              "Summer quarter begins <b>June 23, 2014</b>")
 
             #Date Attribute
             attribute = notice.attributes[0]
             self.assertEquals(attribute.name, "Date")
             self.assertEquals(attribute.data_type, "date")
+            self.assertEquals(attribute.get_value(), future_end.strftime("%Y-%m-%d"))
 
             #String Attribute
             attribute = notice.attributes[3]
@@ -41,7 +60,64 @@ class SWSNotice(TestCase):
             self.assertEquals(notice.custom_category, "Uncategorized")
 
 
+            notice = notices[2]
+            self.assertEquals(notice.notice_category, "StudentDAD")
+            self.assertEquals(notice.notice_type, "EstPd1RegDate")
+            attribute = notice.attributes[0]
+            self.assertEquals(attribute.name, "Date")
+            self.assertEquals(attribute.data_type, "date")
+            self.assertEquals(attribute.get_value(), next_week.strftime("%Y-%m-%d"))
+
+            notice = notices[3]
+            self.assertEquals(notice.notice_category, "StudentALR")
+            self.assertEquals(notice.notice_type, "PreRegNow")
+            attribute = notice.attributes[0]
+            self.assertEquals(attribute.name, "Date")
+            self.assertEquals(attribute.data_type, "date")
+            self.assertEquals(attribute.get_value(), week.strftime("%Y-%m-%d"))
+
+            notice = notices[4]
+            self.assertEquals(notice.notice_category, "StudentALR")
+            self.assertEquals(notice.notice_type, "PreRegNow")
+            attribute = notice.attributes[0]
+            self.assertEquals(attribute.name, "Date")
+            self.assertEquals(attribute.data_type, "date")
+            self.assertEquals(attribute.get_value(), week.strftime("%Y-%m-%d"))
+
+            notice = notices[4]
+            self.assertEquals(notice.notice_category, "StudentALR")
+            self.assertEquals(notice.notice_type, "PreRegNow")
+            attribute = notice.attributes[0]
+            self.assertEquals(attribute.name, "Date")
+            self.assertEquals(attribute.data_type, "date")
+            self.assertEquals(attribute.get_value(), week.strftime("%Y-%m-%d"))
+
+            notice = notices[5]
+            self.assertEquals(notice.notice_category, "StudentALR")
+            self.assertEquals(notice.notice_type, "IntlStuCheckIn")
+            self.assertEquals(len(notice.attributes), 0)
+
             notice = notices[7]
             self.assertEquals(notice.notice_category, "StudentGEN")
             self.assertEquals(notice.notice_type, "AcctBalance")
+            attribute = notice.attributes[0]
+            self.assertEquals(attribute.name, "Date")
+            self.assertEquals(attribute.data_type, "date")
+            self.assertEquals(attribute.get_value(), next_week.strftime("%Y-%m-%d"))
+
+            notice = notices[8]
+            self.assertEquals(notice.notice_category, "StudentDAD")
+            self.assertEquals(notice.notice_type, "TuitDue")
+            attribute = notice.attributes[0]
+            self.assertEquals(attribute.name, "Date")
+            self.assertEquals(attribute.data_type, "date")
+            self.assertEquals(attribute.get_value(), next_week.strftime("%Y-%m-%d"))
+
+            notice = notices[12]
+            self.assertEquals(notice.notice_category, "StudentDAD")
+            self.assertEquals(notice.notice_type, "QtrEnd")
+            attribute = notice.attributes[0]
+            self.assertEquals(attribute.name, "Date")
+            self.assertEquals(attribute.data_type, "date")
+            self.assertEquals(attribute.get_value(), future_end.strftime("%Y-%m-%d"))
 
