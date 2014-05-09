4e5e189a1bd5ed72aeb4b3b690042b7973d19813
fanglin@u.washington.edu <fanglin@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2014-05-09 21:29:24 +0000

    Correction of the personal financial data type.
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@787 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/test/sws/financial.py
+++ restclients/test/sws/financial.py
@@ -10,12 +10,12 @@ class SWSFinance(TestCase):
                 RESTCLIENTS_PWS_DAO_CLASS='restclients.dao_implementation.pws.File'):
 
             data = get_account_balances_by_regid("9136CCB8F66711D5BE060004AC494FFE")
-            self.assertEquals(data.tuition_accbalance, 12345.00)
-            self.assertEquals(data.pce_accbalance, 0)
+            self.assertEquals(data.tuition_accbalance, "12345.00")
+            self.assertEquals(data.pce_accbalance, "0.00")
 
             data = get_account_balances_by_regid("FE36CCB8F66711D5BE060004AC494F31")
-            self.assertEquals(data.tuition_accbalance, 12345.00)
-            self.assertEquals(data.pce_accbalance, 345.00)
+            self.assertEquals(data.tuition_accbalance, "12345.00")
+            self.assertEquals(data.pce_accbalance, "345.00")
 
 
 
