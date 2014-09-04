2640ab0d807048ae13766be14cd07efa7797292a
jlaney@u.washington.edu <jlaney@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2014-09-04 18:39:01 +0000

    fix term tests
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@908 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/test/canvas/terms.py
+++ restclients/test/canvas/terms.py
@@ -5,13 +5,14 @@ from restclients.canvas import Canvas
 class CanvasTestTerms(TestCase):
     def test_term(self):
         with self.settings(
-                RESTCLIENTS_CANVAS_DAO_CLASS='restclients.dao_implementation.canvas.File'):
+                RESTCLIENTS_CANVAS_DAO_CLASS='restclients.dao_implementation.canvas.File',
+                RESTCLIENTS_CANVAS_ACCOUNT_ID='12345'):
             canvas = Canvas()
 
-            sis_term_id = "2012-summer"
+            sis_term_id = "2013-summer"
 
             term = canvas.get_term_by_sis_id(sis_term_id)
 
-            self.assertEquals(term.term_id, 810, "Has proper term id")
-            self.assertEquals(term.name, "Summer 2012", "Has proper name")
+            self.assertEquals(term.term_id, 3845, "Has proper term id")
+            self.assertEquals(term.name, "Summer 2013", "Has proper name")
             self.assertEquals(term.sis_term_id, sis_term_id, "Has proper sis id")
