3af80b35046ca207fcfcbdc0babdf0d2767fe06f
jlaney@u.washington.edu <jlaney@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2014-09-22 17:39:35 +0000

    XHTML response is an error response
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@940 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/r25/__init__.py
+++ restclients/r25/__init__.py
@@ -3,17 +3,24 @@ from restclients.exceptions import DataFailureException
 from lxml import etree
 
 
-nsmap = {"r25": "http://www.collegenet.com/r25"}
+nsmap = {"r25": "http://www.collegenet.com/r25",
+         "xhtml": "http://www.w3.org/1999/xhtml"}
 
 
 def get_resource(url):
     """
     Issue a GET request to R25 with the given url
-    and return a response in xml format.
-    :returns: http response with content in xml
+    and return a response as an etree element.
     """
     response = R25_DAO().getURL(url, {"Accept": "text/xml"})
     if response.status != 200:
         raise DataFailureException(url, response.status, response.data)
 
-    return etree.fromstring(response.data.strip())
+    tree = etree.fromstring(response.data.strip())
+
+    # XHTML response is an error response
+    xhtml = tree.xpath("//xhtml:html", namespaces=nsmap)
+    if len(xhtml):
+        raise DataFailureException(url, 500, response.data)
+
+    return tree
