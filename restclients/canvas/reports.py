c012e3d0c99d262b0c875af598fa51ba14729f01
jlaney@u.washington.edu <jlaney@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2014-09-23 21:31:25 +0000

    adds method for sis_export course report
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@944 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/canvas/reports.py
+++ restclients/canvas/reports.py
@@ -113,6 +113,16 @@ class Reports(Canvas):
         return self.create_report(ReportType.PROVISIONING, account_id, term_id,
                                   params)
 
+    def create_course_sis_export_report(self, account_id, term_id=None,
+                                        params={}):
+        """
+        Convenience method for create_report, for creating a course sis export
+        report.
+        """
+        params["courses"] = True
+        return self.create_report(ReportType.SIS_EXPORT, account_id, term_id,
+                                  params)
+
     def create_unused_courses_report(self, account_id, term_id=None):
         """
         Convenience method for create_report, for creating an unused courses
