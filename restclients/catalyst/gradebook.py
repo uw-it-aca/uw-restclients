16ea0e7455704f421b81a0a7062f91597a634189
jlaney@u.washington.edu <jlaney@u.washington.edu@3f86a6b1-f777-4bc2-bf3d-37c8dbe90857>
2014-10-15 19:39:49 +0000

    adds another catalyst gradebook function
    git-svn-id: https://svn.cac.washington.edu/svn/restclients/trunk@984 3f86a6b1-f777-4bc2-bf3d-37c8dbe90857

--- restclients/catalyst/gradebook.py
+++ restclients/catalyst/gradebook.py
@@ -3,10 +3,28 @@ Interface for interacting with Catalyst GradeBook
 """
 
 from restclients.sws import encode_section_label
-from restclients.exceptions import DataFailureException
 from restclients.models.catalyst import GradebookParticipant
-from restclients.dao import Catalyst_DAO
-import json
+from restclients.catalyst import get_resource
+
+
+def get_participants_for_gradebook(gradebook_id, person=None):
+    """
+    Returns a list of gradebook participants for the passed gradebook_id and
+    person.
+    """
+    url = "/rest/gradebook/v1/book/%s/participants" % gradebook_id
+    headers = {}
+
+    if person is not None:
+        headers["X-UW-Act-as"] = person.uwnetid
+
+    data = get_resource(url, headers)
+
+    participants = []
+    for pt in data["participants"]:
+        participants.append(_participant_from_json(pt))
+
+    return participants
 
 
 def get_participants_for_section(section, person=None):
@@ -15,29 +33,24 @@ def get_participants_for_section(section, person=None):
     """
     section_label = encode_section_label(section.section_label())
     url = "/rest/gradebook/v1/section/%s/participants" % section_label
+    headers = {}
 
-    headers = { "Accept": "application/json" }
     if person is not None:
         headers["X-UW-Act-as"] = person.uwnetid
 
-    response = Catalyst_DAO().getURL(url, headers)
-
-    if response.status != 200:
-        raise DataFailureException(url, response.status, response.data)
-
-    try:
-        data = json.loads(response.data)
-    except ValueError as ex:
-        raise DataFailureException(url, response.status, response.data)
+    data = get_resource(url, headers)
 
     participants = []
     for pt in data["participants"]:
-        participant = GradebookParticipant()
-        participant.participant_id = pt["participant_id"]
-        participant.class_grade = pt["class_grade"]
-        participant.notes = pt["notes"]
-        participant.person_id = pt["person_id"]
-
-        participants.append(participant)
+        participants.append(_participant_from_json(pt))
 
     return participants
+
+
+def _participant_from_json(data):
+    participant = GradebookParticipant()
+    participant.participant_id = data["participant_id"]
+    participant.class_grade = data["class_grade"]
+    participant.notes = data["notes"]
+    participant.person_id = data["person_id"]
+    return participant
