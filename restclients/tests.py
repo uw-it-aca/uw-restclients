e10c696fd3125a15ca9b27baed6949f7dfbf3e19
Fang Lin <fanglin@uw.edu>
2015-03-02 18:02:43 -0800

    update tests (add digitlib.curric)

--- restclients/tests.py
+++ restclients/tests.py
@@ -4,6 +4,7 @@ from restclients.test.uwnetid.subscription import EmailForwardingTest
 from restclients.test.util.date_formator import formatorTest
 from restclients.test.hfs.idcard import HfsTest
 from restclients.test.library.mylibinfo import MyLibInfoTest
+from restclients.test.digitlib.curric import DigitLibTest
 
 from restclients.test.sws.compatible import SWSTest
 from restclients.test.sws.financial import SWSFinance
