cab98d93bcb17616e2b768d90924545d0cfb641a
Fang Lin <fanglin@uw.edu>
2015-07-14 14:31:27 -0700

    Handle the case where the system is null.

--- restclients/grad/leave.py
+++ restclients/grad/leave.py
@@ -11,6 +11,9 @@ from restclients.grad import get_resource, datetime_from_string
 PREFIX = "/services/students/v1/api/leave?id="
 
 
+logger = logging.getLogger(__name__)
+
+
 def get_leave_by_regid(regid):
     sws_person = get_person_by_regid(regid)
     if sws_person is None:
@@ -19,12 +22,16 @@ def get_leave_by_regid(regid):
 
 
 def get_leave_by_syskey(system_key):
+    if system_key is None:
+        logger.info("get_leave_by_syskey abort, key is None!")
+        return None
     url = "%s%s" % (PREFIX, system_key)
     return _process_json(json.loads(get_resource(url)))
 
 
 def _process_json(data):
     """
+    return a list of GradLeave objects.
     """
     requests = []
     for item in data:
