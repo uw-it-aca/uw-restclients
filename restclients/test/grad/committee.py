1859282bd8ec9acc63581fbd90f95c4ac76ed712
Fang Lin <fanglin@uw.edu>
2015-08-20 14:38:37 -0700

    Resolve MUWM-3192 - update restclient grad model to support this new requirement.

--- restclients/test/grad/committee.py
+++ restclients/test/grad/committee.py
@@ -43,6 +43,9 @@ class CommitteeTest(TestCase):
             self.assertEqual(members[0].dept, "Epidemiology - Public Health")
             self.assertEqual(members[0].email, "nnn@u.washington.edu")
             self.assertEqual(members[0].status, "active")
+            json_data = members[0].json_data()
+            self.assertEqual(json_data["reading_type"],
+                             "reading committee chair")
 
             committee = requests[2]
             self.assertEqual(committee.committee_type,
