bc3f7e83bd35f1a6ae8add35932513c7da47076e
Fang Lin <fanglin@uw.edu>
2015-10-01 21:57:58 -0700

    fix a typo.

--- restclients/test/util/datetime_convertor.py
+++ restclients/test/util/datetime_convertor.py
@@ -19,7 +19,7 @@ class DatetimeConvertorTest(TestCase):
 
     def test_convert_to_end_of_day(self):
         self.assertEquals(convert_to_end_of_day(date(2012, 2, 28)),
-                          datetime(2013, 2, 29, 0, 0, 0))
+                          datetime(2012, 2, 29, 0, 0, 0))
 
         self.assertEquals(
             convert_to_end_of_day(datetime(2012, 2, 28, 10, 10, 10)),
