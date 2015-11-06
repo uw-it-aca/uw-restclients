7d546c4815b234097ee9eec2acfc98577d5dc444
Fang Lin <fanglin@uw.edu>
2015-11-06 13:23:50 -0800

    Resolve MUWM-3414 - correct the add fund url.

--- restclients/test/hfs/idcard.py
+++ restclients/test/hfs/idcard.py
@@ -5,6 +5,11 @@ from restclients.hfs.idcard import get_hfs_accounts
 from restclients.exceptions import DataFailureException
 from decimal import  *
 
+
+ADD_FUND_URL =\
+    "https://www.hfs.washington.edu/olco/Secure/AccountSummary.aspx"
+
+
 class HfsTest(TestCase):
 
     def test_get_hfs_accounts(self):
@@ -15,21 +20,24 @@ class HfsTest(TestCase):
             hfs_acc = get_hfs_accounts("javerage")
             self.assertEquals(hfs_acc.student_husky_card.last_updated,
                               datetime(2014, 6, 2, 15, 17, 16))
-            self.assertEquals(hfs_acc.student_husky_card.balance, Decimal('1.23'))
-            self.assertEquals(hfs_acc.student_husky_card.add_funds_url,
-                              "https://www.hfs.washington.edu/olco")
+            self.assertEquals(hfs_acc.student_husky_card.balance,
+                              Decimal('1.23'))
+            self.assertEquals(
+                hfs_acc.student_husky_card.add_funds_url,
+                ADD_FUND_URL)
 
             self.assertEquals(hfs_acc.employee_husky_card.last_updated,
                               datetime(2014, 5, 19, 14, 16, 26))
-            self.assertEquals(hfs_acc.employee_husky_card.balance, Decimal('0.56'))
+            self.assertEquals(hfs_acc.employee_husky_card.balance,
+                              Decimal('0.56'))
             self.assertEquals(hfs_acc.employee_husky_card.add_funds_url,
-                              "https://www.hfs.washington.edu/olco")
+                              ADD_FUND_URL)
 
             self.assertEquals(hfs_acc.resident_dining.last_updated,
                               datetime(2014, 6, 1, 13, 15, 36))
             self.assertEquals(hfs_acc.resident_dining.balance, Decimal('7.89'))
             self.assertEquals(hfs_acc.resident_dining.add_funds_url,
-                              "https://www.hfs.washington.edu/olco")
+                              ADD_FUND_URL)
 
 
     def test_get_hfs_empty_account(self):
@@ -51,15 +59,17 @@ class HfsTest(TestCase):
             hfs_acc = get_hfs_accounts("jnew")
             self.assertIsNotNone(hfs_acc.student_husky_card)
             self.assertIsNone(hfs_acc.student_husky_card.last_updated)
-            self.assertEquals(hfs_acc.student_husky_card.balance, Decimal('0.0'))
+            self.assertEquals(hfs_acc.student_husky_card.balance,
+                              Decimal('0.0'))
 
             self.assertIsNone(hfs_acc.employee_husky_card)
 
-            self.assertEquals(hfs_acc.resident_dining.balance, Decimal('777.89'))
+            self.assertEquals(hfs_acc.resident_dining.balance,
+                              Decimal('777.89'))
             self.assertEquals(hfs_acc.resident_dining.last_updated,
                                datetime(2014, 5, 17, 13, 15, 36))
             self.assertEquals(hfs_acc.resident_dining.add_funds_url,
-                              "https://www.hfs.washington.edu/olco")
+                              ADD_FUND_URL)
 
 
     def test_invalid_user(self):
@@ -68,24 +78,31 @@ class HfsTest(TestCase):
             'restclients.dao_implementation.hfs.File'):
 
             #Testing error message in a 200 response
-            self.assertRaises(DataFailureException, get_hfs_accounts, "invalidnetid")
-            self.assertRaises(DataFailureException, get_hfs_accounts, "invalidnetid123")
+            self.assertRaises(DataFailureException,
+                              get_hfs_accounts, "invalidnetid")
+            self.assertRaises(DataFailureException,
+                              get_hfs_accounts, "invalidnetid123")
 
             try:
                 get_hfs_accounts("none")
             except DataFailureException as ex:
-                self.assertEquals(ex.msg, "UWNetID none not found in IDCard Database.")
+                self.assertEquals(ex.msg,
+                                  "UWNetID none not found in IDCard Database.")
 
-
+            MSG = "%s%s%s" % (
+                "Input for this method must be either a valid UWNetID ",
+                "or two nine-digit Student and ",
+                "Faculty/Staff/Employee ID numbers, comma-separated.")
             try:
                 get_hfs_accounts("invalidnetid")
             except DataFailureException as ex:
-                self.assertEquals(ex.msg, "Input for this method must be either a valid UWNetID or two nine-digit Student and Faculty/Staff/Employee ID numbers, comma-separated.")
-
+                self.assertEquals(
+                    ex.msg, MSG)
 
     def test_float_parsing(self):
         with self.settings(
             RESTCLIENTS_HFS_DAO_CLASS=
             'restclients.dao_implementation.hfs.File'):
             hfs_acc = get_hfs_accounts("jbothell")
-            self.assertEquals(hfs_acc.student_husky_card.balance, Decimal('5.1'))
+            self.assertEquals(hfs_acc.student_husky_card.balance,
+                              Decimal('5.1'))
