9e8eb6a584c1c667d0a2d46b2a6316dfc355dc9a
Jim Laney <jlaney@uw.edu>
2016-01-07 14:33:10 -0800

    enables retrying based on status code

--- restclients/test/util/retry.py
+++ restclients/test/util/retry.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from restclients.exceptions import DataFailureException
 from restclients.util.retry import retry
 
 
@@ -17,6 +18,9 @@ class UnexpectedError(Exception):
 class RetryTest(TestCase):
 
     def test_bad_args(self):
+        self.assertRaises(TypeError, retry, RetryableError, tries=None)
+        self.assertRaises(ValueError, retry, RetryableError, delay=None)
+        self.assertRaises(ValueError, retry, RetryableError, backoff=None)
         self.assertRaises(ValueError, retry, RetryableError, tries=-1)
         self.assertRaises(ValueError, retry, RetryableError, delay=0)
         self.assertRaises(ValueError, retry, RetryableError, backoff=0)
@@ -87,3 +91,76 @@ class RetryTest(TestCase):
 
         self.assertRaises(UnexpectedError, raise_unexpected_error)
         self.assertEqual(self.counter, 1)
+
+    def test_no_retry_with_status(self):
+        self.counter = 0
+
+        @retry(DataFailureException, status_codes=[500], tries=4, delay=0.1)
+        def not_found():
+            self.counter += 1
+            raise DataFailureException('/', 404, '')
+
+        self.assertRaises(DataFailureException, not_found)
+        self.assertEqual(self.counter, 1)
+
+    def test_retry_once_with_status(self):
+        self.counter = 0
+
+        @retry(DataFailureException, status_codes=[500], tries=4, delay=0.1)
+        def fails_once():
+            self.counter += 1
+            if self.counter < 2:
+                raise DataFailureException('/', 500, '')
+            else:
+                return 'success'
+
+        r = fails_once()
+        self.assertEqual(r, 'success')
+        self.assertEqual(self.counter, 2)
+
+    def test_limit_reached_with_status(self):
+        self.counter = 0
+
+        @retry(DataFailureException, status_codes=[500], tries=4, delay=0.1)
+        def always_fails():
+            self.counter += 1
+            raise DataFailureException('/', 500, '')
+
+        self.assertRaises(DataFailureException, always_fails)
+        self.assertEqual(self.counter, 4)
+
+    def test_multiple_status_codes(self):
+        self.counter = 0
+
+        @retry(DataFailureException, status_codes=[500, 503, 504], tries=4, delay=0.1)
+        def raise_multiple_exceptions():
+            self.counter += 1
+            if self.counter == 1:
+                raise DataFailureException('/', 500, '')
+            elif self.counter == 2:
+                raise DataFailureException('/', 503, '')
+            else:
+                return 'success'
+
+        r = raise_multiple_exceptions()
+        self.assertEqual(r, 'success')
+        self.assertEqual(self.counter, 3)
+
+    def test_multiple_exceptions_and_multiple_status_codes(self):
+        self.counter = 0
+
+        @retry((DataFailureException, RetryableError), status_codes=[500, 503, 504], tries=4, delay=0.1)
+        def raise_multiple_exceptions():
+            self.counter += 1
+            if self.counter == 1:
+                raise DataFailureException('/', 500, '')
+            elif self.counter == 2:
+                raise DataFailureException('/', 503, '')
+            elif self.counter == 3:
+                raise RetryableError()
+            else:
+                return 'success'
+
+        r = raise_multiple_exceptions()
+        self.assertEqual(r, 'success')
+        self.assertEqual(self.counter, 4)
--- restclients/util/retry.py
+++ restclients/util/retry.py
@@ -1,25 +1,32 @@
+from restclients.exceptions import DataFailureException
 import math
 import time
 
 
-def retry(ExceptionToCheck, tries=4, delay=3, backoff=2):
+def retry(ExceptionToCheck, tries=4, delay=3, backoff=2, status_codes=[]):
     """
     Decorator function for retrying the decorated function,
     using an exponential or fixed backoff.
 
     Original: https://wiki.python.org/moin/PythonDecoratorLibrary#Retry
 
-    Modified to check for a passed exception.
+    ExceptionToCheck: the exception to check. Can be a tuple of
+        exceptions to check
+    tries: number of times to try (not retry) before giving up
+    delay: initial delay between tries in seconds
+    backoff: backoff multiplier
+    status_codes: list of http status codes to check for retrying, only applies
+         when ExceptionToCheck is a DataFailureException
     """
-    if backoff <= 0:
-        raise ValueError("backoff must be greater than 0")
+    if backoff is None or backoff <= 0:
+        raise ValueError("backoff must be a number greater than 0")
 
     tries = math.floor(tries)
     if tries < 0:
-        raise ValueError("tries must be 0 or greater")
+        raise ValueError("tries must be a number 0 or greater")
 
-    if delay <= 0:
-        raise ValueError("delay must be greater than 0")
+    if delay is None or delay <= 0:
+        raise ValueError("delay must be a number greater than 0")
 
     def deco_retry(f):
         def f_retry(*args, **kwargs):
@@ -27,10 +34,24 @@ def retry(ExceptionToCheck, tries=4, delay=3, backoff=2):
             while mtries > 1:
                 try:
                     return f(*args, **kwargs)
+
                 except ExceptionToCheck as err:
-                    time.sleep(mdelay)
-                    mtries -= 1
-                    mdelay *= backoff
+                    if (type(err) is DataFailureException and
+                            len(status_codes)):
+                        if err.status in status_codes:
+                            time.sleep(mdelay)
+                            mtries -= 1
+                            mdelay *= backoff
+                        else:
+                            raise
+
+                    else:
+                        time.sleep(mdelay)
+                        mtries -= 1
+                        mdelay *= backoff
+
             return f(*args, **kwargs)
+
         return f_retry
+
     return deco_retry
