30fe10af9ca1c73205d1491d9b35b40e7237f197
Patrick Michaud <pmichaud@uw.edu>
2016-02-10 10:29:05 -0800

    Trying travis deployments to pypi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup
 import setuptools
 
 setup(name='UW-RestClients',
-      version='1.0.4',
+      version='1.0.5',
       license = "Apache 2.0",
       author = "UW-IT ACA",
       author_email = "pmichaud@uw.edu",
