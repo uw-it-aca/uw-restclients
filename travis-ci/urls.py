c72442a3cf2a3755bd21d9d36f3b603cce9e5f1f
Patrick Michaud <pmichaud@uw.edu>
2016-10-18 15:12:45 -0700

    django 1.10 urls compat for travis urls

--- travis-ci/urls.py
+++ travis-ci/urls.py
@@ -1,8 +1,17 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+import os
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'project.views.home', name='home'),
-    url(r'^admin/', include(admin.site.urls)),
-)
+version = os.environ.get("DJANGO_VERSION", "1.6")
+
+major, minor = version.split(".")
+
+if major == 1 and minor < 10:
+    from django.conf.urls import patterns, include, url
+    from django.contrib import admin
+    urlpatterns = patterns('',
+        # Examples:
+        # url(r'^$', 'project.views.home', name='home'),
+        # url(r'^blog/', include('blog.urls')),
+    #    url(r'^', include('userservice.urls')),
+    )
+else:
+    urlpatterns = []
