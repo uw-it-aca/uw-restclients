66545f623db02244883ef97e7a8d26879f82d84f
Patrick Michaud <pmichaud@uw.edu>
2016-10-28 14:57:44 -0700

    Moving to named urls

--- restclients/urls.py
+++ restclients/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns(
     '',
-    url(r'view/(\w+)/(.*)$', 'restclients.views.proxy'),
+    url(r'view/(\w+)/(.*)$',
+        'restclients.views.proxy',
+        name="restclients_proxy"),
     )
