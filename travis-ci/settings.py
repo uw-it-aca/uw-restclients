946d287c95a41eb10dee9c7a8df7a6368dbb7869
Patrick Michaud <pmichaud@uw.edu>
2016-10-31 10:09:22 -0700

    Dropping remote user middleware for travis

--- travis-ci/settings.py
+++ travis-ci/settings.py
@@ -54,15 +54,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.RemoteUserMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.RemoteUserBackend',
-#    'django.contrib.auth.backends.ModelBackend',
 )
 
 ROOT_URLCONF = 'travis-ci.urls'
