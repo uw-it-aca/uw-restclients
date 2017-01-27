c6453be987dbd4917d8a52d2ddd9fab00536236f
Fang Lin <fanglin@uw.edu>
2017-01-27 15:39:04 -0800

    use get_timeout

--- restclients/dao_implementation/mailman.py
+++ restclients/dao_implementation/mailman.py
@@ -3,11 +3,9 @@ Contains Mailman DAO implementations.
 """
 
 from django.conf import settings
+from restclients.dao_implementation import get_timeout
 from restclients.dao_implementation.live import get_con_pool, get_live_url
-from restclients.dao_implementation.mock import get_mockdata_url,\
-    convert_to_platform_safe, post_mockdata_url, delete_mockdata_url,\
-    put_mockdata_url, patch_mockdata_url, read_resp_data
-from django.conf import settings
+from restclients.dao_implementation.mock import get_mockdata_url
 
 
 class File(object):
@@ -15,23 +13,21 @@ class File(object):
     The File DAO implementation returns generally static content.  Use this
     DAO with this configuration:
 
-    RESTCLIENTS_MAILMAN_DAO_CLASS =
-    'restclients.dao_implementation.mailman.File'
+    RESTCLIENTS_MAILMAN_DAO_CLASS =\
+        'restclients.dao_implementation.mailman.File'
     """
     def getURL(self, url, headers):
         return get_mockdata_url("mailman", "file", url, headers)
 
 
 MAILMAN_MAX_POOL_SIZE = 10
-MAILMAN_SOCKET_TIMEOUT = 15
 
 
 class Live(object):
     """
-    This DAO provides real data.  It requires further configuration, e.g.
+    This DAO provides real data. It requires further configuration, e.g.
     RESTCLIENTS_MAILMAN_HOST=''
-    RESTCLIENTS_MAILMAN_CERT_FILE='.../cert.cert',
-    RESTCLIENTS_MAILMAN_KEY_FILE='.../certs_key.key',
+    RESTCLIENTS_MAILMAN_KEY=''
     """
     pool = None
     host = None
@@ -40,15 +36,18 @@ class Live(object):
         self.set_pool()
 
     def set_pool(self):
-        if Live.pool is None or Live.host is None:
+        if Live.host is None:
             Live.host = settings.RESTCLIENTS_MAILMAN_HOST
-            Live.pool = get_con_pool(Live.host,
-                                     verify_https=True,
-                                     max_pool_size=MAILMAN_MAX_POOL_SIZE,
-                                     socket_timeout=MAILMAN_SOCKET_TIMEOUT)
+        if Live.pool is None:
+            Live.pool = get_con_pool(
+                Live.host,
+                max_pool_size=MAILMAN_MAX_POOL_SIZE,
+                socket_timeout=get_timeout('mailman'))
 
     def getURL(self, url, headers):
-        return get_live_url(Live.pool, 'GET',
-                            Live.host, url,
+        return get_live_url(Live.pool,
+                            'GET',
+                            Live.host,
+                            url,
                             headers=headers,
                             service_name='mailman')
