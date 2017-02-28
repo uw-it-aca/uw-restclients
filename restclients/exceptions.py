aacff755f556b8e05557392148556ce5e93cb010
Patrick Michaud <pmichaud@uw.edu>
2017-02-28 09:57:33 -0800

    Remove/import from uw_hfs

--- restclients/exceptions.py
+++ restclients/exceptions.py
@@ -1,6 +1,7 @@
 """
 Contains the custom exceptions used by the restclients.
 """
+from restclients_core.exceptions import DataFailureException
 
 
 class PhoneNumberRequired(Exception):
@@ -81,20 +82,3 @@ class InvalidCanvasSection(Exception):
 class InvalidGradebookID:
     """Exception for invalid gradebook id."""
     pass
-
-
-class DataFailureException(Exception):
-    """
-    This exception means there was an error fetching content
-    in one of the rest clients.  You can get the url that failed
-    with .url, the status of the error with .status, and any
-    message with .msg
-    """
-    def __init__(self, url, status, msg):
-        self.url = url
-        self.status = status
-        self.msg = msg
-
-    def __str__(self):
-        return ("Error fetching %s.  Status code: %s.  Message: %s." %
-                (self.url, self.status, self.msg))
--- restclients/util/date_formator.py
+++ restclients/util/date_formator.py
@@ -1,7 +1,10 @@
-import re
-from datetime import datetime, timedelta
+from uw_hfs.util import (is_today, past_datetime_str, is_days_ago,
+                         get_past_months_count, get_past_years_count,
+                         get_past_weeks_count, get_total_seconds,
+                         last_midnight, time_str)
 
 
+# Appears to be unused - prune later
 def full_month_date_str(adate):
     """
     Return a string value in the format of
@@ -12,14 +15,7 @@ def full_month_date_str(adate):
                           adate.year)
 
 
-def time_str(adatetime):
-    """
-    Return the format of "at hour:minute [AP]M",
-    where the hour doesn't have a leading zero.
-    """
-    return re.sub(r'^0', '', adatetime.strftime("%I:%M %p"))
-
-
+# Appears to be unused - prune later
 def abbr_month_date_time_str(adatetime):
     """
     Return a date value in the format of
@@ -31,6 +27,8 @@ def abbr_month_date_time_str(adatetime):
                                 time_str(adatetime))
 
 
+
+# Used by the SWS
 def abbr_week_month_day_str(adatetime):
     """
     Return a string consists of abbreviated weekday, abbreviated month,
@@ -41,115 +39,5 @@ def abbr_week_month_day_str(adatetime):
     return "%s %d" % (adatetime.strftime("%a, %b"), adatetime.day)
 
 
-def past_datetime_str(adatetime):
-    """
-    For adatetime is since 12:00AM, return: "Today at H:MM [A]PM"
-    For adatetime is between 12:00AM and 11:59PM on yesterday:
-                                    "Yesterday at 6:23 PM"
-    For adatetime is between 2 and 6 days ago: "[2-6] days ago"
-    For adatetime is 7 days ago: "1 week ago"
-    For adatetime is 8-14 days ago: "Over 1 week ago"
-    For adatetime is 15-21 days ago: "Over 2 weeks ago"
-    For adatetime is 22-28 days ago: "Over 3 weeks ago"
-    For adatetime is 29-56 days ago: "Over 1 month ago"
-    For adatetime is 57-84 days ago: "Over 2 months ago"
-    For adatetime is 85-112 days ago: "Over 3 months ago"
-    and so on, in increments of 28 days, until T-365,
-    at which point: "Over 1 year ago",
-    then after another 365 days "Over 2 years ago", etc
-    """
-    if is_today(adatetime):
-        return "today at %s" % time_str(adatetime)
-
-    if last_midnight() - adatetime <= timedelta(days=7):
-        for day in xrange(1, 8):
-            if is_days_ago(adatetime, day):
-                if day == 1:
-                    return "yesterday at %s" % time_str(adatetime)
-                if day == 7:
-                    return "1 week ago"
-                return "%d days ago" % day
-
-    if last_midnight() - adatetime <= timedelta(days=28):
-        week = get_past_weeks_count(adatetime)
-        if week == 1:
-            return "over %d week ago" % week
-        else:
-            return "over %d weeks ago" % week
-
-    if last_midnight() - adatetime <= timedelta(days=365):
-        month = get_past_months_count(adatetime)
-        if month == 1:
-            return "over %d month ago" % month
-        else:
-            return "over %d months ago" % month
-
-    year = get_past_years_count(adatetime)
-    if year == 1:
-        return "over %d year ago" % year
-    else:
-        return "over %d years ago" % year
 
 
-def last_midnight():
-    """
-    return a datetime of last mid-night
-    """
-    now = datetime.now()
-    return datetime(now.year, now.month, now.day)
-
-
-def is_today(adatetime):
-    """
-    Return true if the adatetime is since this morning 12:00AM
-    """
-    return last_midnight() <= adatetime
-
-
-def is_days_ago(adatetime, days):
-    """
-    :param days: a positive integer.
-    Return true if the adatetime is on the specified days ago
-    """
-    if days == 1:
-        end_time = last_midnight()
-        start_time = end_time - timedelta(days=1)
-    else:
-        start_time = last_midnight() - timedelta(days=days)
-        end_time = start_time + timedelta(days=1)
-    return start_time <= adatetime <= end_time
-
-
-def get_past_weeks_count(adatetime):
-    """
-    Return the number of weeks that the adatetime is over in the past
-    7 days are counted as one week.
-    """
-    duration = last_midnight() - adatetime
-    return get_total_seconds(duration) // get_total_seconds(timedelta(7))
-
-
-def get_past_months_count(adatetime):
-    """
-    Return the number of months that the adatetime is over in the past
-    28 days are counted as one month.
-    """
-    duration = last_midnight() - adatetime
-    return get_total_seconds(duration) // get_total_seconds(timedelta(28))
-
-
-def get_past_years_count(adatetime):
-    """
-    Return the number of years that the adatetime is over in the past
-    365 days are counted as one year.
-    """
-    duration = last_midnight() - adatetime
-    return get_total_seconds(duration) // get_total_seconds(timedelta(365))
-
-
-def get_total_seconds(time_delta):
-    """
-    Returns the total number of seconds in a passed timedelta
-    """
-    return (time_delta.microseconds +
-            (time_delta.seconds + time_delta.days * 24 * 3600) * 10**6) / 10**6
