158d1d6ef136a06d4f1e07582e0a72dc0b47c435
Jim Laney <jlaney@uw.edu>
2017-03-27 15:40:04 -0700

    restoring the signals

--- restclients/signals/rest_request.py
+++ restclients/signals/rest_request.py
@@ -1,2 +1,21 @@
-from rc_django.signals.rest_request import (
-    rest_request, rest_request_receiver, get_signal)
+try:
+    from status_app.dispatch import dispatch
+    from status_app.models import RawEvent
+except Exception as ex:
+    pass
+from django.utils import timezone
+import django.dispatch
+
+
+rest_request = django.dispatch.Signal(providing_args=['url', 'request_time', 'hostname', 'service_name'])
+
+
+def rest_request_receiver(sender, **kwargs):
+    url = kwargs.get('url', None)
+    request_time = kwargs.get('request_time', None)
+    service_name = kwargs.get('service_name', None)
+    hostname = kwargs.get('hostname', None)
+    dispatch('%s_request_interval' % service_name, RawEvent.INTERVAL, timezone.now(), request_time, url, hostname)
+
+def get_signal():
+    return rest_request
--- restclients/signals/success.py
+++ restclients/signals/success.py
@@ -1,2 +1,22 @@
-from rc_django.signals.success import (
-    rest_request_passfail, rest_request_passfail_receiver, get_signal)
+try:
+    from status_app.dispatch import dispatch
+    from status_app.models import RawEvent
+except Exception as ex:
+    pass
+from django.utils import timezone
+import django.dispatch
+
+
+rest_request_passfail = django.dispatch.Signal(providing_args=['url', 'success', 'hostname', 'service_name'])
+
+
+def rest_request_passfail_receiver(sender, **kwargs):
+    url = kwargs.get('url', None)
+    success = kwargs.get('success', False)
+    service_name = kwargs.get('service_name', None)
+    hostname = kwargs.get('hostname', None)
+    dispatch('%s_request_passfail' % service_name, RawEvent.PASS_FAIL, timezone.now(), success, url, hostname)
+
+def get_signal():
+    return rest_request_passfail
+
