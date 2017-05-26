60ed0a42bf76c53916108c3bf5d093f50d9ba95d
Fang Lin <fanglin@uw.edu>
2017-05-25 22:05:09 -0700

    pep8

--- restclients/bridge/user.py
+++ restclients/bridge/user.py
@@ -2,6 +2,6 @@ from uw_bridge.user import (admin_id_url, admin_uid_url, author_id_url,
                             ADMIN_URL_PREFIX, AUTHOR_URL_PREFIX,
                             author_uid_url, add_user, change_uid,
                             delete_user, delete_user_by_id,
-                            get_all_users, get_user, get_user_by_id,\
+                            get_all_users, get_user, get_user_by_id,
                             replace_uid, restore_user, restore_user_by_id,
                             update_user, get_regid_from_custom_fields)
